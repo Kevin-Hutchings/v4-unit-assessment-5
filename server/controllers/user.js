facfe716ec015fa095a51ec1b592044bbeff478f
Kevin-Hutchings <ktmhutchings@gmail.com>
2021-08-31 20:18:50 -0600

    endpoints tested and mostly functional

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,14 +4,14 @@ const register = async (req, res) => {
    const { username, password } = req.body;
    const profile_pic = `https://robohash.org/${username}.png`;
    const db = req.app.get('db');
-   const result = await db.find_user_by_username([username]);
+   const result = await db.user.find_user_by_username([username]);
    const existingUser = result[0];
 
    if(existingUser) {
       res.status(409).json('Username taken')
    } else {
       const hash = bcrypt.hashSync(password);
-      const registeredUser = await db.create_user([username, hash, profile_pic]);
+      const registeredUser = await db.user.create_user([username, hash, profile_pic]);
       const user = registeredUser[0];
       req.session.user = {
          username: user.username,
@@ -25,13 +25,13 @@ const register = async (req, res) => {
 const login = async (req, res) => {
    const { username, password } = req.body;
    const db = req.app.get('db');
-   const foundUser = await db.find_user_by_username([username]);
+   const foundUser = await db.user.find_user_by_username([username]);
    const user = foundUser[0];
 
    if(!user) {
       res.status(401).json('User not found')
    } else {
-      const isAuthenticated = bcrypt.compareSync(password, user.hash);
+      const isAuthenticated = bcrypt.compareSync(password, user.password);
       if(!isAuthenticated) {
          res.status(403).json('Incorrect password');
       } else {
